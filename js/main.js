d574dfd43ea3cb5a4348a9f581d06170b2090c7f
David <66249515+Davids-dir@users.noreply.github.com>
2020-09-23 22:55:16 +0200

    Testing P1 select fighter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 // Creamos las variables con las que vamos a trabajar
+player1 = ""
+player2 = ""
+
+// Array con el pool de personajes para escoger
+pool = ["ryu", "chunli", "ken", "akuma"]
+
+// Funcion de seleccion de personaje
+function Selection (a) {
+    for (let i = 0; i < pool.lenght; i ++);
+        
 
 
-arrayDisplay = []
-arrayChar = []
+}
 
 
 
