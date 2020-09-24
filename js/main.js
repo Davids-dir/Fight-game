52f020eec313d26adef8ef384e4b75704474e8b0
David <66249515+Davids-dir@users.noreply.github.com>
2020-09-25 01:17:02 +0200

    When player select fighter, portrait change to grayescale.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
-// Creamos las variables con las que vamos a trabajar
-player1 = ""
-player2 = ""
+// Inicio la estructura del programa
+// Creamos la clase de los luchadores
+class Fighter {
 
-// Array con el pool de personajes para escoger
-pool = ["Ryu", "Chunli", "Ken", "Akuma"]
-
-// Funcion de seleccion de personaje
-function Selection (a) {
-    for (let i = 0; i < pool.lenght; i ++);
+    constructor (name, life, strength) {
+        this.name = name;
+        this.life = life;
+        this.strength = strength;
+    }    
 }
 
+// Añadimos los luchadores asignando las propiedades de la clase 'Fighter'
+let Ryu = new Fighter ("Ryu", 180, 23);
+let Ken = new Fighter ("Ken", 160, 25);
+let Chunli = new Fighter ("Chunli", 145, 20);
+let Akuma = new Fighter ("Akuma", 190, 28);
 
+// Creamos el array con los luchadores del pool
+pool = [Ryu, Ken, Chunli, Akuma];
 
-console.log(pool)
+// Creo el contador para la condicion
+
+// Creando la asignacion del luchador al jugador
+// Cuando el jugador haga su seleccion marcamos al luchador en gris y lo inhabilitamos de la seleccion
+const Select = (a) => {
+        let pickChar = document.getElementById (a).innerHTML; 
+        document.getElementById(a).style.filter = 'grayscale(100%)';
+
+}
