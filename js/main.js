9df0fd65cc52a36dc7a13a892dc103d091400910
David <66249515+Davids-dir@users.noreply.github.com>
2020-09-27 18:50:08 +0200

    Edit HTML, CSS and JS:
    HTML end for phase 3, added font for name and HP of fighters, in JS added lines to function selChar.Wwhen players choose their fighters, show the HP and name in phase 3.

--- js/main.js
+++ js/main.js
@@ -14,10 +14,10 @@ class Fighter {
 }
 
 // Añadimos los luchadores asignando las propiedades de la clase 'Fighter'
-let Ryu = new Fighter("Ryu", 180, 23);
-let Ken = new Fighter("Ken", 160, 25);
-let Chunli = new Fighter("Chunli", 145, 20);
-let Akuma = new Fighter("Akuma", 190, 28);
+let Ryu = new Fighter("Ryu", 190, 25);
+let Ken = new Fighter("Ken", 180, 26);
+let Chunli = new Fighter("Chunli", 170, 23);
+let Akuma = new Fighter("Akuma", 200, 28);
 
 // Creamos el array con los luchadores del pool asignandolos mediante 'clave:valor'
 pool = {
@@ -44,6 +44,8 @@ const selChar = (a) => {
                 document.getElementById(a).style.filter = 'grayscale(100%)';
                 document.getElementById("fighterP1").innerHTML = '<img src="../img/' + a + '-move.webp" />';
                 document.getElementById("player1Stage").innerHTML = '<img src="../img/' + a + '-move.webp" />';
+                document.getElementById("nameP1").innerHTML = player1.name
+                document.getElementById("lifeBarP1").innerText = player1.life;
                 document.getElementById("textSelect").innerHTML = 'Select player Two';
                 contador++;
             }
@@ -55,12 +57,18 @@ const selChar = (a) => {
                 document.getElementById("fighterP2").innerHTML = '<img src="../img/' + a + '-move.webp" />';
                 document.getElementById("player2Stage").innerHTML = '<img src="../img/' + a + '-move.webp" />';
                 document.getElementById("textSelect").innerHTML = 'Press Fight';
+                document.getElementById("nameP2").innerText = player2.name;
+                document.getElementById("lifeBarP2").innerHTML = player2.life;
                 contador++;
                 document.getElementById("buttonFight").style.display ="block";
             }
     }
 }
 
+// Funcion para mostrar el nombre y vida de los luchadores en la Fase 3
+/*function FightInfo (a, b) {
+    document.getElementById("nameP1").innerHTML = a.name;
+    document.getElementById("nameP2").innerHTML = b.name;
+}
 
-
-
+FightInfo(player1, player2);*/
