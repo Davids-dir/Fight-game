dc54228105a390c2d7785c6168a1e55b67392636
David <66249515+Davids-dir@users.noreply.github.com>
2020-09-28 00:29:56 +0200

    Edit JS:
    Create Reset function to start again the game.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // Declaro las variables con las que vamos a trabajar
-var contador = 0;
-var player1;
-var player2;
-
+let contador = 0;
+let player1;
+let player2;
+let player1Life;
+let player2Life;
 
 // Creamos la clase de los luchadores
 class Fighter {
@@ -20,7 +21,9 @@ let Chunli = new Fighter("Chunli", 170, 23);
 let Akuma = new Fighter("Akuma", 200, 28);
 
 // Creamos el array con los luchadores del pool asignandolos mediante 'clave:valor'
-pool = {
+
+
+let pool = {
     'Ryu': Ryu,
     'Ken': Ken,
     'Chunli': Chunli,
@@ -40,6 +43,7 @@ const selChar = (a) => {
         
             if (contador < 1) {
                 player1 = pool[a];
+                player1Life = player1.life;
                 delete pool[a];
                 document.getElementById(a).style.filter = 'grayscale(100%)';
                 document.getElementById("fighterP1").innerHTML = '<img src="../img/' + a + '-move.webp" />';
@@ -52,6 +56,7 @@ const selChar = (a) => {
 
             else {
                 player2 = pool[a];
+                player2Life = player2.life;
                 delete pool[a];
                 document.getElementById(a).style.filter = 'grayscale(100%)';
                 document.getElementById("fighterP2").innerHTML = '<img src="../img/' + a + '-move.webp" />';
@@ -65,8 +70,6 @@ const selChar = (a) => {
     }
 }
 
-
-
 // Funcion que alterna los golpes entre los jugadores
 const Hit = () => {
 
@@ -79,7 +82,8 @@ const Hit = () => {
         document.getElementById('lifeBarP2').innerHTML = player2.life;
         
         if (player2.life <= 0) {
-            player2.life = 0;
+            document.getElementById('winnerText').innerText = player1.name + " " + "Win"
+            document.getElementById('winnerPortrait').innerHTML = '<img src="../img/' + player1.name + '-win.webp" height="160" width="140" />';
             document.getElementById('phase4').style.display = 'block';
             document.getElementById('phase3').style.display = 'none';
         }
@@ -90,10 +94,35 @@ const Hit = () => {
         document.getElementById('lifeBarP1').innerHTML = player1.life;
         
         if (player1.life <= 0) {
-            player1.life = 0;
+            document.getElementById('winnerText').innerText = player2.name + " " + "Win"
+            document.getElementById('winnerPortrait').innerHTML = '<img src="../img/' + player2.name + '-win.webp" height="160" width="140" />';
             document.getElementById('phase4').style.display = 'block';
             document.getElementById('phase3').style.display = 'none';
         } 
     }
 }
 
+// Funcion para realizar el reset
+const Reset = () => {
+
+    document.getElementById('phase4').style.display = 'none';
+    document.getElementById('phase1').style.display = 'block';
+
+    contador = 0;
+    document.getElementById("fighterP1").innerHTML = '<img src="" />';
+    document.getElementById("fighterP2").innerHTML = '<img src="" />';
+
+    document.getElementById(player1.name).style.filter = 'grayscale(0%)';
+    document.getElementById(player2.name).style.filter = 'grayscale(0%)';
+
+    document.getElementById("buttonFight").style.display ="none";
+    
+    player1.life = player1Life;
+    player2.life = player2Life;
+
+    pool[player1.name] = player1;
+    pool[player2.name] = player2;
+
+    document.getElementById("lifeBarP1").innerHTML = player1.life;
+    document.getElementById("lifeBarP2").innerHTML = player2.life;
+}
